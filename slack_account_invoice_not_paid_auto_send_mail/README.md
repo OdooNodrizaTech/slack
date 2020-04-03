932b5c16d8931169a409a4c16d9bb60a5fc29b11
Informatica Arelux <informatica@arelux.com>
2020-04-03 14:48:43 +0200

    slack_account_invoice_not_paid_auto_send_mail

---
+++ slack_account_invoice_not_paid_auto_send_mail/README.md
@@ -0,0 +1,1 @@
+Implementa el addon account_invoice_auto_send_mail para enviar por Slack un mensaje
---
+++ slack_account_invoice_not_paid_auto_send_mail/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Slack Account Invoice Not Paid Auto Send Mail',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'account_invoice_not_paid_auto_send_mail', 'slack'],
+    'data': [],    
+    'installable': True,
+    'auto_install': False,    
+}
