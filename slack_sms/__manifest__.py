9fd76f3f0425cf88071c2e8b80387746dbe4c6ad
Informatica Arelux <informatica@arelux.com>
2020-04-06 07:57:46 +0200

    mejoras

--- slack_sms/__manifest__.py
+++ slack_sms/__manifest__.py
@@ -6,7 +6,7 @@
     'website': 'https://nodrizatech.com/',
     'category': 'Tools',
     'license': 'AGPL-3',
-    'depends': ['base', 'sms_arelux', 'slack'],
+    'depends': ['base', 'sms', 'slack'],
     'data': [
         'data/slack_data.xml'
     ],    
