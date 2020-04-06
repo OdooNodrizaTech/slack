cd97a0c75a94875471a4eeedc067443df69017b1
Informatica Arelux <informatica@arelux.com>
2020-04-06 09:01:37 +0200

    slack_sale_orders_generate_invoice

--- slack_sale_orders_generate_invoice/models/account_invoice.py
+++ slack_sale_orders_generate_invoice/models/account_invoice.py
@@ -1,28 +1,37 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models, api, _
-
 import logging
 _logger = logging.getLogger(__name__)
+
+from odoo import api, fields, models
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+import decimal
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    @api.one 
+    def action_auto_open(self):
+        return_item = super(AccountInvoice, self).action_auto_open()
+        #action_send_account_invoice_create_message_slack
+        self.action_send_account_invoice_create_message_slack()        
+        #return
+        return return_item
+    
     @api.one    
-    def action_auto_create_message_slack(self):
-        res = super(AccountInvoice, self).action_auto_create_message_slack()
-        
+    def action_send_account_invoice_create_message_slack(self):        
         web_base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-                                                                        
+                                                        
         attachments = [
-            {
-                "title": 'Se ha creado la factura de OniAd automaticamente',
-                "text": self.number,
-                "fallback": "Ver factura "+str(web_base_url)+"/web?#id="+str(self.id)+"&view_type=form&model=account.invoice",
-                "color": "#36a64f",
+            {                    
+                "title": 'Se ha creado la factura automaticamente',
+                "text": self.number,                        
+                "color": "#36a64f",                                             
+                "fallback": "Ver factura "+str(self.number)+' '+str(web_base_url)+"/web?#id="+str(self.id)+"&view_type=form&model=account.invoice",                                    
                 "actions": [
                     {
                         "type": "button",
-                        "text": "Ver factura",
+                        "text": "Ver factura "+str(self.number),
                         "url": str(web_base_url)+"/web?#id="+str(self.id)+"&view_type=form&model=account.invoice"
                     }
                 ],
@@ -33,13 +42,13 @@ class AccountInvoice(models.Model):
                         'short': True,
                     },
                     {
-                        "title": "Importe",
-                        "value": str(self.amount_total)+' '+self.currency_id.symbol,
+                        "title": "Pedido",
+                        "value": self.origin,
                         'short': True,
                     }
-                ],
+                ],                    
             }
-        ]
+        ]            
         
         slack_message_vals = {
             'attachments': attachments,
@@ -49,4 +58,4 @@ class AccountInvoice(models.Model):
         }                        
         slack_message_obj = self.env['slack.message'].sudo().create(slack_message_vals)
         
-        return res            
+        return res                                
