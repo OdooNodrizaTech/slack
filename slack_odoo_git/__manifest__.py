4d6fbdf3ca2ff08ffce44d94be45d1efbb30d7b2
Informatica Arelux <informatica@arelux.com>
2020-04-22 14:07:02 +0200

    slack_odoo_git

---
+++ slack_odoo_git/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Slack Odoo Git',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'slack', 'odoo_git'],
+    'data': [],    
+    'installable': True,
+    'auto_install': False,    
+}
---
+++ slack_odoo_git/models/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+import git_repository
