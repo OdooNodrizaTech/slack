da29588ae806d8e759e4f2d1fa3593886293dafe
Informatica Arelux <informatica@arelux.com>
2020-05-08 10:25:39 +0200

    v12
    slack_arelux
    slack_sms

---
+++ slack_arelux/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import slack_channel_daily_report
+from . import stock_picking
+from . import survey_mail_compose_message 
---
+++ slack_sms/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Slack Sms',
+    'version': '12.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base', 'aws_sms', 'slack'],
+    'data': [
+        'data/slack_data.xml'
+    ],    
+    'installable': True,
+    'auto_install': False,    
+}
