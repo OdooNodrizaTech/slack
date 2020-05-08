547bc1f008d5dcf7cf5feb30eb45672f0490c244
Informatica Arelux <informatica@arelux.com>
2020-05-08 13:20:46 +0200

    slack_payment

---
+++ slack_payment/models/payment_transaction.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, tools
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class PaymentTransaction(models.Model):
+    _inherit = 'payment.transaction'
+
+    @api.model
+    def action_payment_transaction_done_error(self, error):
+        attachments = [
+            {                    
+                "title": 'Incidencia en Transaccion',
+                "text": str(error['error']),                        
+                "color": "#ff0000",                
+                "fields": [
+                    {
+                        "title": "Referencia",
+                        "value": str(error['reference']),
+                        'short': True,
+                    },                    
+                    {
+                        "title": "Entidad",
+                        "value": str(error['acquirer_id_name']),
+                        'short': True,
+                    },                    
+                ],                    
+            }
+        ]        
+        #slack_message_vals
+        slack_message_vals = {
+            'attachments': attachments,
+            'model': self._inherit,
+            'res_id': self.id,
+            'as_user': True,
+            'channel': str(self.env['ir.config_parameter'].sudo().get_param('slack_log_channel'))                                                         
+        }                        
+        slack_message_obj = self.env['slack.message'].sudo().create(slack_message_vals)
