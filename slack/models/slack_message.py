ac894088353ae3b46c1fb87cc096ac1fba25548b
Informatica Arelux <informatica@arelux.com>
2020-05-13 09:55:27 +0200

    fix

--- slack/models/slack_message.py
+++ slack/models/slack_message.py
@@ -14,34 +14,35 @@ class SlackMessage(models.Model):
     def create(self, values):        
         channel = self.env['ir.config_parameter'].sudo().get_param('slack_log_channel')
         api_token = tools.config.get('slack_bot_user_oauth_access_token')
-        #api_token
-        if 'api_token' in values:
-            api_token = values['api_token']
-        #default
-        msg = ''
-        attachments = []
-        #msg
-        if 'msg' in values:
-            msg = values['msg']        
-        #attachments
-        if 'attachments' in values:
-            attachments = values['attachments']
-        #channel            
-        if 'channel' in values:
-            channel = values['channel']                    
-        #SlackClient
-        sc = slack.WebClient(api_token)
-        #sc = SlackClient(api_token)
-        result = sc.api_call(
-            "chat.postMessage", 
-            channel=channel, 
-            text=msg, 
-            attachments=attachments, 
-            username='Odoo'
-        )
-        if 'error' in result:
-            _logger.info({
-                'channel': channel,
-                'error': result['error'] 
-            })
+        if api_token!=None:
+            #api_token
+            if 'api_token' in values:
+                api_token = values['api_token']
+            #default
+            msg = ''
+            attachments = []
+            #msg
+            if 'msg' in values:
+                msg = values['msg']        
+            #attachments
+            if 'attachments' in values:
+                attachments = values['attachments']
+            #channel            
+            if 'channel' in values:
+                channel = values['channel']                    
+            #SlackClient
+            sc = slack.WebClient(api_token)
+            #sc = SlackClient(api_token)
+            result = sc.api_call(
+                "chat.postMessage", 
+                channel=channel, 
+                text=msg, 
+                attachments=attachments, 
+                username='Odoo'
+            )
+            if 'error' in result:
+                _logger.info({
+                    'channel': channel,
+                    'error': result['error'] 
+                })
                                                 
