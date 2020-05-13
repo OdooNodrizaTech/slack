b5b6faf83a3ff9fc1f754fe821f777fd91013ed3
Informatica Arelux <informatica@arelux.com>
2020-05-13 14:18:44 +0200

    sqs_dead_letter

---
+++ slack_sqs_dead_letter/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Slack SQS Dead Letter',
+    'version': '10.0.1.0.0',    
+    'author': 'Odoo Nodriza Tech (ONT)',
+    'website': 'https://nodrizatech.com/',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'depends': ['base','slack', 'ir_attachment_s3'],
+    'data': [
+        'data/ir_config_parameter.xml',
+        'data/ir_cron.xml',
+    ],
+    'installable': True,
+    'auto_install': False,    
+}
