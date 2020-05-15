8f6a080977e3f92faf58eb01b20e4491660ec471
Informatica Arelux <informatica@arelux.com>
2020-05-15 14:33:29 +0200

    fix

--- slack_mail/models/mail_mail.py
+++ slack_mail/models/mail_mail.py
@@ -8,39 +8,41 @@ class MailMail(models.Model):
     _inherit = 'mail.mail'
     
     @api.multi
-    def _postprocess_sent_message(self, mail_sent=True):
-        res = super(MailMail, self)._postprocess_sent_message(mail_sent)
-        if mail_sent!=True:
-            attachments = [
-                {                    
-                    "title": 'Se ha producido un error al enviar el email',
-                    "text": self.subject.encode('utf-8'),                         
-                    "color": "#ff0000",
-                    "fields": [                    
-                        {
-                            "title": "Razon del fallo",
-                            "value": self.failure_reason.encode('utf-8'),
-                            'short': True,
-                        },
-                        {
-                            "title": "Remitente",
-                            "value": self.email_from.encode('utf-8'),
-                            'short': True,
-                        },
-                        {
-                            "title": self.model,
-                            "value": self.res_id,
-                            'short': True,
-                        },                        
-                    ],                                                                                    
-                }
-            ]
-            
-            slack_message_vals = {
-                'attachments': attachments,
-                'model': self._inherit,
-                'res_id': self.id,                                                         
-            }                        
-            slack_message_obj = self.env['slack.message'].sudo().create(slack_message_vals)
+    def _postprocess_sent_message(self, success_pids, failure_reason=False, failure_type=None):
+        res = super(MailMail, self)._postprocess_sent_message(success_pids, failure_reason, failure_type)
+        if failure_reason==True:
+            for item in self:
+                if item.state=='exception':
+                    attachments = [
+                        {                    
+                            "title": 'Se ha producido un error al enviar el email',
+                            "text": item.subject,                         
+                            "color": "#ff0000",
+                            "fields": [                    
+                                {
+                                    "title": "Razon del fallo",
+                                    "value": item.failure_reason,
+                                    'short': True,
+                                },
+                                {
+                                    "title": "Remitente",
+                                    "value": item.email_from,
+                                    'short': True,
+                                },
+                                {
+                                    "title": item.model,
+                                    "value": item.res_id,
+                                    'short': True,
+                                },                        
+                            ],                                                                                    
+                        }
+                    ]
+                    
+                    slack_message_vals = {
+                        'attachments': attachments,
+                        'model': self._inherit,
+                        'res_id': item.id,                                                         
+                    }                        
+                    slack_message_obj = self.env['slack.message'].sudo().create(slack_message_vals)
         #return                                                                                                            
-        return res
+        return res    
